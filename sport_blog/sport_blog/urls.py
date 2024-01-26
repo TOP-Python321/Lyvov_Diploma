e742e5b046e6c2f8ae3965bb7cc0b2aef3286203
Alexey Lyvov <angel.redangel2009@yandex.ru>
2024-01-26 11:18:54 +0300

    Настройка внешнего вида

--- sport_blog/sport_blog/urls.py
+++ sport_blog/sport_blog/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 from users.views import user_login, user_logout, register
